589e5a89ea5758fe35afc8ebd4dcbf60ffa55d34
David Brownman <beamneocube@gmail.com>
2015-05-27 00:35:02 -0700

    move object creation into methods

--- test/test_account.rb
+++ test/test_account.rb
@@ -6,11 +6,11 @@ def create_netflix
   RIQ.account('54e6542fe4b01ad3b7362bc4')
 end
 
-def create_blank
+def create_blank_account
   RIQ.account
 end
 
-def create_data
+def create_data_account
   RIQ.account({name: 'Glengarry', field_values: {'0' => 3}})
 end
 
@@ -21,26 +21,24 @@ describe RIQ::Account do
 
   describe '#new' do
     it 'should get account' do
-      # VCR.use_cassette('account#new') do
-        @netflix = create_netflix
-        @netflix.name.must_equal 'Netflix'
-      # end
+      @netflix = create_netflix
+      @netflix.name.must_equal 'Netflix'
     end
 
     it 'should make blank account' do
-      @a = create_blank
+      @a = create_blank_account
       @a.wont_be_nil
     end
 
     it 'should take a data hash' do 
-      @dat = create_data
+      @dat = create_data_account
       @dat.name.wont_be_nil
     end
   end
 
   describe '#save' do 
     it 'should create new account' do
-      @a = create_blank
+      @a = create_blank_account
       @a.name = 'Delete Test Inc'
       @a.field_value(2, '1')
       @a.save
@@ -53,7 +51,7 @@ describe RIQ::Account do
     it 'should fetch a field value' do 
       @netflix = create_netflix
       @netflix.field_value(2).wont_be_nil
-      @dat = create_data
+      @dat = create_data_account
       @dat.field_value(0).wont_be_nil
     end
   end
